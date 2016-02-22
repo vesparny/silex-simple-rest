b377d9432c060ead23ef3c847a920b162688bbb7
jdiaz <jdiaz@alertlogic.com>
2016-02-22 16:30:38 -0500

    Added missing files from first commit

---
+++ src/App/Rest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App;
+
+use Silex\Application;
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Carbon\Carbon;
+
+final class Rest
+{
+    /**
+     * @var Application
+     */
+    private $api;
+
+    /**
+     * @var array $dbConfig Database configuration params
+     */
+    private $dbConfig;
+
+    /**
+     * @var array $dbConfig
+     */
+    public $globalConfig;
+
+    /**
+     * Rest constructor.
+     *
+     * @param string $environment
+     */
+    public function __construct($environment = 'prod')
+    {
+        $this->api = new \Silex\Application();
+        $this->loadConfig($environment);
+        $this->registerApiHandler();
+
+    }
+
+    /**
+     * @param string $environment possible values are dev, prod
+     */
+    public function loadConfig($environment)
+    {
+        $app = $this->api;
+
+        require __DIR__ . "/../../resources/config/$environment.php";
+
+        $this->dbConfig     = $dbConfig;
+        $this->globalConfig = $globalConfig;
+    }
+
+    /**
+     * Register api
+     */
+    public function registerApiHandler()
+    {
+        #handle CORS PREFLIGHT request
+        $this->api->before(function (Request $request) {
+            if ($request->getMethod() === "OPTIONS") {
+                $response = new Response();
+                $response->headers->set("Access-Control-Allow-Origin", "*");
+                $response->headers->set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+                $response->headers->set("Access-Control-Allow-Headers", "Content-Type");
+                $response->setStatusCode(200);
+
+                return $response->send();
+            }
+        }, Application::EARLY_EVENT);
+
+
+        #handling CORS response with right headers
+        $this->api->after(function (Request $request, Response $response) {
+            $response->headers->set("Access-Control-Allow-Origin", "*");
+            $response->headers->set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        });
+
+        $this->api->before(function (Request $request) {
+            if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+                $data = json_decode($request->getContent(), true);
+                $request->request->replace(is_array($data) ? $data : array());
+            }
+        });
+
+        $this->registerModules();
+
+        $this->api->error(function (\Exception $e, $code) {
+            $this->api[ 'monolog' ]->addError($e->getMessage());
+            $this->api[ 'monolog' ]->addError($e->getTraceAsString());
+
+            return new JsonResponse(
+                array("statusCode" => $code, "message" => $e->getMessage(), "stacktrace" => $e->getTraceAsString())
+            );
+        });
+
+        $this->registerEndpoints();
+    }
+
+    /**
+     * Register modules
+     */
+    public function registerModules()
+    {
+        $this->api->register(new ServiceControllerServiceProvider());
+        $this->api->register(new DoctrineServiceProvider(), $this->dbConfig);
+
+        $this->api->register(
+            new HttpCacheServiceProvider(),
+            array("http_cache.cache_dir" => ROOT_PATH . "/storage/cache",)
+        );
+
+        $this->api->register(new MonologServiceProvider(), array(
+            "monolog.logfile" => ROOT_PATH . "/storage/logs/" . Carbon::now(
+                    $this->globalConfig[ 'timeZone' ]
+                )->format($this->globalConfig[ 'dateFormat' ]) . ".log",
+            "monolog.level"   => $this->api[ "log.level" ],
+            "monolog.name"    => "application"
+        ));
+
+    }
+
+    /**
+     * Register endpoints
+     */
+    public function registerEndpoints()
+    {
+
+        //TODO fetch endpoints from database
+        /**
+         * Register any new services for endpoints
+         */
+        $controllerMapping = array(
+            'companies'      => 'Companies',
+            'customers'      => 'Customers',
+            'customer-types' => 'CustomerTypes',
+            'remissions'     => 'Remissions',
+            'waste'          => 'Waste',
+            'waste-types'    => 'WasteTypes'
+        );
+
+        foreach ($controllerMapping as $service => $controller) {
+            $routing = new \App\Routing($this->api, $service, $controller);
+            $routing->register();
+        }
+    }
+
+    /**
+     * Run the API
+     *
+     * @return mixed
+     */
+    public function run()
+    {
+        return $this->api[ 'http_cache' ]->run();
+    }
+}
---
+++ src/App/Routing.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App;
+
+use Silex\Application;
+
+class Routing
+{
+    /**
+     * @var Application
+     */
+    private $api;
+
+    /**
+     * @var string
+     */
+    private $controller;
+
+    /**
+     * Routing constructor.
+     *
+     * @param Application $app
+     * @param bool      $service
+     * @param string      $mappedClass
+     */
+    public function __construct(Application $app, $service , $mappedClass)
+    {
+        $this->api         = $app;
+        $this->mappedClass = $mappedClass;
+        $this->service     = $service;
+    }
+
+    /**
+     * @return object
+     */
+    private function getController()
+    {
+
+        $controller = "App\\Controllers\\" . $this->mappedClass . "Controller";
+        $controller = new $controller($this->api, $this->api[ $this->service.".service" ]);
+
+        $this->api[ "$this->service.controller" ] = $this->api->share(function () use ($controller) {
+            return $controller;
+        });
+
+
+        return $controller;
+
+    }
+
+    public function registerService()
+    {
+        $this->api[ $this->service.".service" ] = $this->api->share(function () {
+
+            $service = "App\\Services\\" . $this->mappedClass . "Service";
+
+            return new $service($this->api);
+        });
+
+    }
+
+
+    /**
+     * Register the controller endpoints
+     */
+    public function register()
+    {
+        $this->registerService();
+        $controller = $this->getController();
+        $endpoints  = $controller->endpoints();
+        $api        = $this->api[ "controllers_factory" ];
+
+        //TODO modify api call loader to lazy load the endpoints
+
+        foreach ($endpoints as $method => $endpoint) {
+            self::registerEndpoints($api, $method, $endpoint);
+        }
+
+        $this->api->mount($this->api[ "api.endpoint" ] . '/' . $this->api[ "api.version" ], $api);
+
+    }
+
+    /**
+     * Register endpoints
+     *
+     * @param Application $api
+     * @param string      $method
+     * @param array       $endpoints
+     */
+    public static function registerEndpoints(&$api, $method, array $endpoints)
+    {
+        foreach ($endpoints as $endpoint => $callback) {
+            $api->{$method}($endpoint, $callback);
+        }
+    }
+
+}
