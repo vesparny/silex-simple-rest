b514bb46ce4b5802d81c0899015f1b4b6c93d1d7
jdiaz <jdiaz@alertlogic.com>
2016-02-24 14:04:06 -0500

    Fixed dev.php configuration, 'Single responsibility principle'

--- resources/config/dev.php
+++ resources/config/dev.php
@@ -1,8 +1,21 @@
 <?php
-require __DIR__ . '/prod.php';
-$app['debug'] = true;
-$app['log.level'] = Monolog\Logger::DEBUG;
-$app['db.options'] = array(
-  'driver' => 'pdo_sqlite',
-  'path' => realpath(ROOT_PATH . '/app.db'),
+
+date_default_timezone_set('Europe/London');
+define("ROOT_PATH", __DIR__ . "/..");
+
+$app['debug']       = true;
+$app['log.level']   = \Monolog\Logger::DEBUG;
+$app['api.version'] = "v1";
+$app['api.endpoint'] = "/api";
+
+$dbConfig = array(
+    "db.options" => array(
+        "driver" => "pdo_sqlite",
+        'path'   => realpath(ROOT_PATH . '/app.db')
+    )
+);
+
+$globalConfig = array(
+    'timeZone'   => 'Europe/London',
+    'dateFormat' => 'Y-m-d'
 );
