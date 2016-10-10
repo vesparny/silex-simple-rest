d16cc73266bb6477f207a8c3841dbc5e126ee573
Fabian Lachman <fabianfabian@gmail.com>
2016-10-10 23:18:51 +0200

    Update prod config with examples

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -2,10 +2,22 @@
 $app['log.level'] = Monolog\Logger::ERROR;
 $app['api.version'] = "v1";
 $app['api.endpoint'] = "/api";
+
+/**
+ * SQLite database file
+ */
 $app['db.options'] = array(
-  "driver" => "pdo_mysql",
-  "user" => "root",
-  "password" => "root",
-  "dbname" => "prod_db",
-  "host" => "prod_host",
+    'driver' => 'pdo_sqlite',
+    'path' => realpath(ROOT_PATH . '/app.db'),
 );
+
+/**
+ * MySQL
+ */
+//$app['db.options'] = array(
+//  "driver" => "pdo_mysql",
+//  "user" => "root",
+//  "password" => "root",
+//  "dbname" => "prod_db",
+//  "host" => "prod_host",
+//);
