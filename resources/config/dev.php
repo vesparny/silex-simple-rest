1396019b5cd594e649c4f3e765ffe4513abf74e4
vmoura-vogel <vinicius.silveira@vogeltelecom.com>
2017-09-17 23:01:43 -0300

    Config adjustments....

--- resources/config/dev.php
+++ resources/config/dev.php
@@ -1,8 +1,9 @@
 <?php
+
 require __DIR__ . '/prod.php';
+
+/**
+ * Log configurations
+ */
 $app['debug'] = true;
 $app['log.level'] = Monolog\Logger::DEBUG;
-$app['db.options'] = array(
-  'driver' => 'pdo_sqlite',
-  'path' => realpath(ROOT_PATH . '/app.db'),
-);
--- resources/config/prod.php
+++ resources/config/prod.php
@@ -1,10 +1,24 @@
 <?php
+
+/**
+ * Default timezone
+ */
+$timezone = 'America/Sao_Paulo';
+date_default_timezone_set($timezone);
+
+/**
+ * Log configurations
+ */
 $app['log.level'] = Monolog\Logger::ERROR;
+
+/**
+ * API configurations
+ */
 $app['api.version'] = "v1";
 $app['api.endpoint'] = "/api";
 
 /**
- * SQLite database file
+ * DB Configurations
  */
 $app['db.options'] = array(
     'driver' => 'pdo_sqlite',
@@ -12,12 +26,24 @@ $app['db.options'] = array(
 );
 
 /**
- * MySQL
+ * DB Configurations Doctrine
  */
-//$app['db.options'] = array(
-//  "driver" => "pdo_mysql",
-//  "user" => "root",
-//  "password" => "root",
-//  "dbname" => "prod_db",
-//  "host" => "prod_host",
-//);
+$dbConfig = array(
+    "db.options" => $app["db.options"]
+);
+
+/**
+ * HTTP Cache Configurations
+ */
+$httpConfig = array(
+    "http_cache.cache_dir" => ROOT_PATH . "/storage/cache"
+);
+
+/**
+ * Monolog Configurations
+ */
+$monologConfig = array(
+    "monolog.logfile" => ROOT_PATH . "/storage/logs/" . Carbon\Carbon::now($timezone)->format("Y-m-d") . ".log",
+    "monolog.level" => $app["log.level"],
+    "monolog.name" => "application"
+);
